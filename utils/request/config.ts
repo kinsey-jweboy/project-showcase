aa4eabd12618b1fe6cd207ec19a6362ccceabddf
jweboy <jweboy0630@gmail.com>
2024-11-07 22:02:42 +0800

    feat: 更新接口地址

--- utils/request/config.ts
+++ utils/request/config.ts
@@ -6,7 +6,7 @@ export interface RequestConfig extends axios.AxiosRequestConfig {
 }
 
 export const config: RequestConfig = {
-  baseURL: 'https://jweboy-api.lovemypet.asia',
+  baseURL: 'https://jweboy-api.jweboy.asia',
   // baseURL: 'http://localhost:4000',
   responseType: 'json',
   timeout: 15000, // 15s超时
