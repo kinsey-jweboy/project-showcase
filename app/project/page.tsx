43379c7abe9e83cbd5873632df049d4762f972a9
jweboy <jweboy0630@gmail.com>
2025-07-30 22:12:23 +0800

    feat: 更新首页接口逻辑

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Link } from '@nextui-org/link';
 import { title, subtitle } from '@/components/primitives';
 import { Card, CardBody, CardFooter } from '@nextui-org/card';
@@ -12,22 +10,16 @@ import { Image } from '@nextui-org/image';
 import React from 'react';
 import { isValidUrl } from '@/utils';
 
-type Params = {
-  type: React.Key;
-  isPublic: boolean;
-};
+async function ProjectPage() {
+  const { data } = await fetch(
+    process.env.NEXT_PUBLIC_SITE_URL + '/api/project/list',
+  ).then((res) => res.json());
 
-function Home() {
-  const { data, isMutating, trigger } = useSWRMutation<
-    List<Project>,
-    Error,
-    string,
-    Params
-  >('/project/list', postFetcher);
+  console.log('render', data);
 
-  const handleTabChange = (value: React.Key) => {
-    trigger({ type: value, isPublic: true });
-  };
+  // const handleTabChange = (value: React.Key) => {
+  //   trigger({ type: value, isPublic: true });
+  // };
 
   return (
     <section className="flex flex-col items-center justify-center gap-4">
@@ -39,13 +31,13 @@ function Home() {
         </h2>
       </div>
       <div className="flex w-full gap-4">
-        <Tabs aria-label="Tabs" isVertical onSelectionChange={handleTabChange}>
+        {/* <Tabs aria-label="Tabs" isVertical onSelectionChange={handleTabChange}>
           <Tab title="小程序（微信、支付宝）" key="miniprogram" />
           <Tab title="PC网站（后台、独立站）" key="pc"></Tab>
           <Tab title="H5落地页（手机端页面）" key="h5"></Tab>
-        </Tabs>
+        </Tabs> */}
         <div className="flex-1">
-          {!isMutating ? (
+          {/* {!isMutating ? (
             <div className="grid grid-cols-3 gap-6">
               {data?.items.map((item, index) => (
                 <Link
@@ -91,11 +83,53 @@ function Home() {
             </div>
           ) : (
             <Spinner />
-          )}
+          )} */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            {data?.map((item, index) => (
+              <Link
+                href={`/project/${item.id}`}
+                target="_blank"
+                className="block"
+                key={index}
+              >
+                <Card shadow="sm" className="h-[350px] group">
+                  <CardBody className="overflow-visible p-0">
+                    <div className="overflow-hidden rounded-large">
+                      {item.type === 'miniprogram' && isValidUrl(item.link) && (
+                        <div className="group-hover:visible invisible flex items-center justify-center absolute z-50 bg-black/50 w-full h-full transition-all duration-100">
+                          <QRCodeSVG value={item.link} size={200} />
+                        </div>
+                      )}
+                      <Image
+                        shadow="sm"
+                        radius="lg"
+                        width="100%"
+                        isZoomed={item.type !== 'miniprogram'}
+                        loading="lazy"
+                        alt={item.name}
+                        className="h-[260px] object-cover"
+                        src={item.images[0]}
+                      />
+                    </div>
+                  </CardBody>
+                  {/* <CardFooter>
+                    <div className="flex flex-col w-full">
+                      <h3 className="text-lg font-medium text-default-700 flex justify-between items-center">
+                        {item.name}
+                      </h3>
+                      <p className="text-small text-default-500 h-10 line-clamp-2">
+                        {item.description}
+                      </p>
+                    </div>
+                  </CardFooter> */}
+                </Card>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </section>
   );
 }
 
-export default Home;
+export default ProjectPage;
